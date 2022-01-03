8d9ce4e5471c44735e4247624a5261caaa577c48
Pablo <pablorsantos15@gmail.com>
2022-01-03 11:37:11 -0300

    feat: challenge complete

--- src/index.js
+++ src/index.js
@@ -73,39 +73,33 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body;
   const { id } = request.params;
 
-  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
-  if (todoIndex === -1) {
+  const userTodo = user.todos.find((todo) => todo.id === id);
+  if (!userTodo) {
     return response.status(404).json({
       error: "Todo not exist",
     });
   }
 
-  user.todos[todoIndex] = {
-    ...user.todos[todoIndex],
-    title,
-    deadline: new Date(deadline),
-  };
+  userTodo.title = title
+  userTodo.deadline = new Date(deadline)
 
-  response.json(user.todos[todoIndex]).status(204);
+  response.json(userTodo).status(204);
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   const { user } = request;
   const { id } = request.params;
 
-  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
-  if (todoIndex === -1) {
+  const userTodo = user.todos.find((todo) => todo.id === id);
+  if (!userTodo) {
     return response.status(404).json({
       error: "Todo not exist",
     });
   }
 
-  user.todos[todoIndex] = {
-    ...user.todos[todoIndex],
-    done: true,
-  };
+  userTodo.done = true
 
-  response.json(user.todos[todoIndex]).status(204);
+  response.json(userTodo).status(204);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
